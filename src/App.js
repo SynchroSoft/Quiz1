3763677c90f9105ec39b34be55527c5157264e52
Reza Teshnizi <reza.teshnizi@synchrogrid.com>
2020-02-07 12:54:00 -0600

    init(Calculator): Add basic components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<div> Basic Calculator </div>
-			</header>
-		</div>
-	);
+import Calculator from "./Calculator";
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<div> Basic Calculator </div>
+				</header>
+				<Calculator />
+			</div>
+		);
+	}
 }
-
-export default App;
