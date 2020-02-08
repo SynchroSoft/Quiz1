5ceb09418ae064218fd5171e809d6c5f1a7024ed
Reza Teshnizi <reza.teshnizi@synchrogrid.com>
2020-02-07 19:12:51 -0600

    feat(App): Add MUI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import * as Mui from "@material-ui/core";
 
 import Calculator from "./Calculator";
 
@@ -10,6 +11,11 @@ export default class App extends React.Component {
 					<div> Basic Calculator </div>
 				</header>
 				<Calculator />
+				{/* The below element is not rendered (obviously) */}
+				{/* This is for Material UI task (see the README.md) */}
+				<div style={{ display: "none" }}>
+					<Mui.Button variant="contained" >Material UI Button example</Mui.Button>
+				</div>
 			</div>
 		);
 	}
