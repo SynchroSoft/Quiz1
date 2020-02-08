6a5ac48f4974cd32d2402dca9f8ad27278beaf17
Reza Teshnizi <reza.teshnizi@synchrogrid.com>
2020-02-07 18:02:26 -0600

    feat(all): Add all of the GUI

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Calculator from "./Calculator";
 export default class App extends React.Component {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
+			<div className="app">
+				<header className="app-header">
 					<div> Basic Calculator </div>
 				</header>
 				<Calculator />
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 export default class Button extends React.Component {
+	forceSpace() {
+		return
+	}
+
 	render() {
 		return (
-			<button>
-				{this.props.text}
+			<button className="calc-button" disabled={this.props.disabled}>
+				{this.props.disabled ? "\u00a0" : this.props.children}
 			</button>
 		);
 	}
